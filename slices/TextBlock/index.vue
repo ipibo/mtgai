9ff7ee60f5bc76f0ea8c19f4bbc6364920bf1f73
Ibo <contact@iboibelings.nl>
2024-02-02 14:24:34 +0100

    Fix import statement in TextBlock component

--- slices/TextBlock/index.vue
+++ slices/TextBlock/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { type Content } from "@prismicio/client";
+import { type Content } from "@prismicio/client"
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
@@ -9,8 +9,8 @@ defineProps(
     "index",
     "slices",
     "context",
-  ]),
-);
+  ])
+)
 </script>
 
 <template>
@@ -18,6 +18,13 @@ defineProps(
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    
+    <!-- Placeholder component for text_block (variation: {{ slice.variation }})
+    Slices -->
+    <div class="bg-black w-[400px] h-full rounded-lg flex flex-col ">
+      <div class="bg-gray-200 rounded-lg text-center py-2 m-2 drop-shadow-lg">
+        {{ slice.primary.title[0].text }}
+      </div>
+      <div class="m-4 bg-indigo-100 rounded-lg p-2 ">{{slice.primary.information[0].text}}</div>
+    </div>
   </section>
 </template>
